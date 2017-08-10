caf958b930fd44c4f8bf5f4626350267da99a85e
John Spicer <john@editd.com>
2017-08-10 16:45:50 +0100

    Remove unnecessary stuff from setup.py and implement change that

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "gmail",
@@ -18,7 +10,6 @@ setup(
     keywords = "google gmail",
     url = "https://github.com/charlierguo/gmail",
     packages=['gmail'],
-    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Communications :: Email",
